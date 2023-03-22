d25bba95edfff586ecfb0f111b0a1a6ebff2ca2e
Admin <makwanadharmishtha44@gmail.com>
2023-03-22 19:42:13 +0530

    setting ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
-// import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:setting_ui/screen/home/provider/home_provider.dart';
 import 'package:setting_ui/screen/home/view/home_screen.dart';
-import 'package:setting_ui/screen/ihome_screen/view/ihome.dart';
+
 
 void main() {
   runApp(
@@ -17,30 +15,12 @@ void main() {
           create: (context) => home_provider(),
         )
       ],
-      child: CupertinoApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: {
-          '/': (p0) => ios_screen(),
+          '/': (p0) => home_screen(),
         },
       ),
     ),
   );
 }
-
-// Widget Android() {
-//   return MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     routes: {
-//       '/': (context) => home_screen(),
-//     },
-//   );
-// }
-//
-// Widget iOS() {
-//   return CupertinoApp(
-//     debugShowCheckedModeBanner: false,
-//     routes: {
-//       '/': (p0) => ios_screen(),
-//     },
-//   );
-// }
